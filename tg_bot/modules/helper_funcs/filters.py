c5200de3a7d9eca7b2f9f31353eeda25e3992c59
sajjadafnan <71611618+sajjadafnan@users.noreply.github.com>
2021-02-11 17:53:35 +0530

    Update filters.py

--- tg_bot/modules/helper_funcs/filters.py
+++ tg_bot/modules/helper_funcs/filters.py
@@ -28,7 +28,7 @@ class CustomFilters(object):
     mime_type = _MimeType
 
     class _HasText(BaseFilter):
-        def filter(self, message: Message):
+        def add(self, message: Message):
             return bool(message.text or message.sticker or message.photo or message.document or message.video)
 
     has_text = _HasText()
